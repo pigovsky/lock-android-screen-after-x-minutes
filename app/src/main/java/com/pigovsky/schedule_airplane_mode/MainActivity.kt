fcc46f00b1815be582d3b2e0eebab1367543f0e0
Jürij Pigowski <iurii.pigovskyi@fh-dortmund.de>
2025-05-26 16:14:40 +0200

    add speech

--- app/src/main/java/com/pigovsky/schedule_airplane_mode/MainActivity.kt
+++ app/src/main/java/com/pigovsky/schedule_airplane_mode/MainActivity.kt
@@ -7,41 +7,57 @@ import android.app.admin.DevicePolicyManager
 import android.content.ComponentName
 import android.content.Context
 import android.content.Intent
+import android.speech.tts.TextToSpeech
+import android.util.Log
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
 import androidx.lifecycle.Observer
+import java.util.Locale
 
 class MainActivity : ComponentActivity() {
 
     private lateinit var devicePolicyManager: DevicePolicyManager
     private lateinit var compName: ComponentName
+    private var canSpeak = false
+
+    val onSpeechInit = TextToSpeech.OnInitListener { status ->
+        if (status == TextToSpeech.SUCCESS) {
+            canSpeak = true
+            textToSpeech!!.setLanguage(Locale.UK)
+            textToSpeech!!.setSpeechRate(1.0f)
+            textToSpeech!!.speak("Привіт, Єва!", TextToSpeech.QUEUE_FLUSH, null, null)
+        } else {
+            canSpeak = false
+        }
+    }
+    var textToSpeech: TextToSpeech? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main) // Replace with your layout
 
+        textToSpeech = TextToSpeech(this, onSpeechInit)
+
         devicePolicyManager = getSystemService(Context.DEVICE_POLICY_SERVICE) as DevicePolicyManager
         compName = ComponentName(this, MyDeviceAdminReceiver::class.java)
 
-        // Example: Button to request admin privileges
-        val enableAdminButton: Button = findViewById(R.id.enable_admin_button)
-        enableAdminButton.setOnClickListener {
-            if (!devicePolicyManager.isAdminActive(compName)) {
-                val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
-                intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName)
-                intent.putExtra(
-                    DevicePolicyManager.EXTRA_ADD_EXPLANATION,
-                    "This app needs device administrator access to lock the screen."
-                )
-                startActivityForResult(
-                    intent,
-                    REQUEST_CODE_ENABLE_ADMIN
-                ) // Define REQUEST_CODE_ENABLE_ADMIN
-            } else {
-                // Admin is already active
-            }
+        // Example: Request admin privileges
+        if (!devicePolicyManager.isAdminActive(compName)) {
+            val intent = Intent(DevicePolicyManager.ACTION_ADD_DEVICE_ADMIN)
+            intent.putExtra(DevicePolicyManager.EXTRA_DEVICE_ADMIN, compName)
+            intent.putExtra(
+                DevicePolicyManager.EXTRA_ADD_EXPLANATION,
+                "This app needs device administrator access to lock the screen."
+            )
+            startActivityForResult(
+                intent,
+                REQUEST_CODE_ENABLE_ADMIN
+            ) // Define REQUEST_CODE_ENABLE_ADMIN
+        } else {
+            // Admin is already active
+            Toast.makeText(this, "Admin is already active", Toast.LENGTH_SHORT).show()
         }
 
         val textView: TextView = findViewById(R.id.textView)
@@ -70,6 +86,12 @@ class MainActivity : ComponentActivity() {
 
         // Observe LiveData for messages from the AlarmReceiver
         AppEvents.alarmMessage.observe(this, Observer { message ->
+            textToSpeech!!.speak(
+                "Єва, інтернет закінчився! Слухай маму і тата!",
+                TextToSpeech.QUEUE_FLUSH,
+                null,
+                null
+            )
             lockScreenButton.isEnabled = true
             textView.text = message
             Toast.makeText(this, "From Alarm: $message", Toast.LENGTH_LONG).show()
@@ -89,6 +111,16 @@ class MainActivity : ComponentActivity() {
                 // Admin enabling failed or was cancelled by user
             }
         }
+    }
+
+    override fun onDestroy() {
+        // Don't forget to shutdown TTS!
+        if (textToSpeech != null) {
+            textToSpeech!!.stop()
+            textToSpeech!!.shutdown()
+            Log.d("TextToSpeech", "TTS Destroyed")
+        }
+        super.onDestroy()
     }
 
     companion object {
